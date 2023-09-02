82ee7d5fc83148da1f1000e5d69de920c9d51c29
Shadowman405 <maximus405@gmail.com>
2023-09-02 21:01:16 +0300

    link storyboard items with code

---
+++ WastedMoney/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  WastedMoney
+//
+//  Created by Maxim Mitin on 2.09.23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginImg: UIImageView!
+    @IBOutlet weak var emailTxtFld: UITextField!
+    @IBOutlet weak var passwordTxtFld: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    //MARK: - Buttons
+    
+
+    @IBAction func loginTaped(_ sender: Any) {
+    }
+    
+    @IBAction func registerTaped(_ sender: Any) {
+    }
+    
+}
+
