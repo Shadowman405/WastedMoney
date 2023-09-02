57fe6391a7d064a5a84cd0be3de70918a4540677
Shadowman405 <maximus405@gmail.com>
2023-09-02 20:56:13 +0300

    Initial Commit

---
+++ WastedMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WastedMoney
+//
+//  Created by Maxim Mitin on 2.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
