41bf313cbce3b33ef2517426b15708cfd2768933
Shadowman405 <maximus405@gmail.com>
2023-09-04 20:50:19 +0300

    add mock data

--- WastedMoney/Controllers/PersonListTableViewController.swift
+++ WastedMoney/Controllers/PersonListTableViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class PersonListTableViewController: UITableViewController {
+    
+    let mockPerson = [Person(name: "Bogdan", lastName: "Bebra", nickName: "Dungeon Master", gender: "Ultramale"), Person(name: "Hoe", lastName: "Honey", nickName: "Twat", gender: "Female")]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,22 +20,20 @@ class PersonListTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return mockPerson.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
+        let person = mockPerson[indexPath.row]
+        cell.textLabel?.text = "\(person.name) \(person.lastName)  aka - \(person.nickName)"
         return cell
     }
-    */
     
 }
---
+++ WastedMoney/Model/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  WastedMoney
+//
+//  Created by Maxim Mitin on 4.09.23.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let lastName: String
+    let nickName: String
+    let gender: String
+}
